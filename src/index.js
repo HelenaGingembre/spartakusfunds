2a96ee57219ae5c4723cd5b1db911418190ea966
Helena Gingembre <e.izotovagingembre@outlook.com>
2023-12-05 12:48:40 +0100

    update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { theme } from "./services/theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/spartakusfunds/">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
